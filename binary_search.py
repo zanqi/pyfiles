947ed5bc79974f5e100f8463b14b12e7486a34dd
Zanqi <zanqi.liang@gmail.com>
2024-12-26 11:46:00 -0800

    binary search

---
+++ binary_search.py
@@ -0,0 +1,22 @@
+def binary_search(arr, target):
+    l, r = 0, len(arr)-1
+
+    # RI: target is within [l, r]
+    while l <= r:
+        m = (l+r)//2
+        if arr[m] == target:
+            return m
+        if arr[m] < target:
+            l = m+1
+        else:
+            r = m-1
+    # l > r, [l, r] is empty. target is not found
+    return -1
+
+
+a = binary_search([1, 2, 3], 3)
+print(a)
+b = binary_search([1, 2, 3], 0)
+print(b)
+c = binary_search([1, 2, 3], 1)
+print(c)
